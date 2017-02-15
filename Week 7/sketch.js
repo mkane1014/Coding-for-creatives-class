16aaa318d08afd149bec46896472c2d4099ee82a
Matthew J Kane <Metta@Matthews-MacBook-Pro.local>
2017-02-14 19:48:03 -0500

    add week 7

---
+++ Week 7/sketch.js
@@ -0,0 +1,43 @@
+var bubbles = [];
+for (var i = 0; i < bubbles.length; i++) {
+	bubble.move[i];
+	bubble.display();
+ }
+
+
+
+var bubble = {
+	for (var i = 0; i < 10000000; i++){
+	
+}
+
+function setup(){
+	createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+	background (50);
+	for (var i = 0; i < bubbles.length; i++) {
+	bubble.move[i];
+	bubble.display();
+	}
+}
+
+function bubble(){
+bubbles [i] } 
+	x:random (0, width),
+	y:random (0,height)
+	display:function() {
+	stroke (255);
+	nofill();
+	rect(this.x, this.y, 24, 24)
+	},
+	move: function() {
+	this.x = this.x + random (-2,2);
+	this.y = this.y = random (-2,2);
+	}
+}
+
+
+
+
