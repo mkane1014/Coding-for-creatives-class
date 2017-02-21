0de7305f2d19632f09339f2c0d8281d24f6abaa5
Matthew J Kane <Metta@Matthews-MacBook-Pro.local>
2017-02-21 08:32:49 -0500

    add week 8.2

---
+++ Week 8.2/tree.js
@@ -0,0 +1,35 @@
+var angle = 0;
+
+var slider;
+
+
+function setup(){
+	createCanvas(600, 600);
+	slider = createSlider(0, Two_PI, PI/4, 0.01);
+}
+
+function draw() {
+	background(0);
+	angle = slider.value();
+	stroke(255);
+	translate(200, height);
+	branch(100);
+
+}
+
+function branch(length) {
+	line(0, 0, 0, - length);
+	translate(0, -length);
+	if (length > 4) {
+	push();
+	rotate(angle);
+	branch(length * 0.67);
+    pop();
+	push();
+	rotate(-angle);
+	branch(len* 0.67);
+	pop();
+
+	
+}
+	}
