a89cf44c171000c97d704e2d53dd0285d90ac70f
Matthew J Kane <Metta@Matthews-MacBook-Pro.local>
2017-02-20 19:39:01 -0500

    add Week 8

--- Week 6.8/sketch.js
+++ Week 6.8/sketch.js
@@ -1,3 +1,5 @@
+/* Credit to Codepen */ 
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
---
+++ Week 8/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+	createCanvas(windowWidth,windowHeight)
+
+}
+
+function draw() {
+	line(30, 20, 85, 20);
+	
+	ellipse(56, 46, 55, 55);
+	
+	line(85, 20, 85, 75);
+
+	}
